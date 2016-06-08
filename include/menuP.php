884fbe3ba35484cbe7836ec6dff9132781b9e6ed
A2MI <A2MI.versionning@gmail.com>
2016-06-08 11:53:39 +0200

    création plateforme

---
+++ include/menuP.php
@@ -0,0 +1,55 @@
+		
+		<!--Fais appel au css via cette ligne de commande-->
+		
+		<link rel='stylesheet'   media='screen and (max-width: 768px)'  type='text/css'  href='../testcss.css' />
+		
+		<!--Fais appel à la méthode css 'bande3' via le div-->
+		
+		<div id='bande3'>
+		<br/>
+			
+			<!--Fais appel au design du menu-->
+			
+			<ul id='menu'>
+			
+				<!--menu site A&M-->
+				
+				<li><br/><a href='../accueil/test2.php'><img src="../images/gif/Pre.gif"   style="margin-left: 110px" onmouseover="this.src='../images/gif/Pre2.gif'" onmouseout="this.src='../images/gif/Pre.gif'" height='100px' width='100px'/></br><font color='white' style="margin-left: 130px">Site A&M</font></a>
+					
+				</li>
+				
+				<!--menu cartes de visite-->
+				
+				<li><br/><a href='../realisation/cartesRT.php'><img src="../images/plateforme/cv.png"  style="margin-left: 72px" onmouseover="this.src='../images/plateforme/cv2.png'" onmouseout="this.src='../images/plateforme/cv.png'" height='100px' width='100px'/></br><font color='white' style="margin-left: 70px">Cartes de visite</font></a>
+				</li>
+			
+				<!--menu flyers-->
+				
+				<li><br/><a href='../realisation/Flyers.php'><img src="../images/plateforme/F.png"  style="margin-left:85px" onmouseover="this.src='../images/plateforme/F2.png'" onmouseout="this.src='../images/plateforme/F.png'" height='100px' width='100px'/></br><font color='white' style="margin-left: 113px">Flyers</font></a>
+				
+				</li>
+				<!--menu logos-->
+				
+				<li><br/><a href='../realisation/Logo.php'><img src="../images/plateforme/L.png"  style="margin-left: 83px" onmouseover="this.src='../images/plateforme/L2.png'" onmouseout="this.src='../images/plateforme/L.png'" height='100px' width='100px'/></br><font color='white' style="margin-left: 115px">Logo</font></a>
+				
+					
+				
+				<!--menu références et réalisations-->
+				
+				<li><br/><a href='../references/referencesR.php'><img src="../images/plateforme/R.png"  style="margin-left: 60px" onmouseover="this.src='../images/plateforme/R2.png'" onmouseout="this.src='../images/plateforme/R.png'" height='100px' width='100px'/></br><font color='white' style="margin-left: 55px">Nos références</font></a>
+					<ul>
+						<li><a><font size='4'>Flyers</font></a>
+							<ul>
+								<li><a href='../references/A4R.php'><font size='4'>A4</font></a></li>
+								<li><a href='../references/A5R.php'><font size='4'>A5</font></a></li>
+								<li><a href='../references/A6R.php'><font size='4'>A6</font></a></li>
+							</ul>
+						</li>
+						<li><a href='../references/logoR.php'><font size='4'>Logos</font></a></li>
+						<li><a href='../references/cartesR.php'><font size='4'>Cartes de visites</font></a></li>
+						<li><a href='../references/etiquetteR.php'><font size='4'>Etiquettes</font></a></li>
+						<li><a href='../references/chartesR.php'><font size='4'>Chartes graphiques</font></a></li>
+					</ul>
+				</li>
+			</ul>
+		</div>
