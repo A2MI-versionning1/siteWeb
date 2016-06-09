8ad79fbdfa10573fcccd400d9078f8d1485a9933
A2MI <A2MI.versionning@gmail.com>
2016-06-09 14:02:36 +0200

    Menu design couleur

--- include/menu2.php
+++ include/menu2.php
@@ -14,26 +14,30 @@
 			
 				<!--menu de la présentation de l'entreprise-->
 				
-				<li><br/><a href='../presentation/presentation2.php'><img src="../images/gif/Pre.gif"   style="margin-left: 95px" onmouseover="this.src='../images/gif/Pre2.gif'" onmouseout="this.src='../images/gif/Pre.gif'" height='100px' width='100px'/></br><font color='white' style="margin-left: 105px">Présentation</font></a>
-					
+				<div id="presentation"><li><br/><a href='../presentation/presentation2.php'><img src="../images/gif/Pre.gif"   style="margin-left: 95px" onmouseover="this.src='../images/gif/Pre2.gif'" onmouseout="this.src='../images/gif/Pre.gif'" height='100px' width='100px'/></br><font color='white' style="margin-left: 105px">Présentation</font></a>
+				</div>
 				</li>
 				
 				<!--menu de notre savoir faire-->
 				
-				// <li><br/><a href='#'><img src="../images/gif/S.gif"  style="margin-left:82px" onmouseover="this.src='../images/gif/S2.gif'" onmouseout="this.src='../images/gif/S.gif'" height='100px' width='100px'/></br><font color='white' style="margin-left: 89px">Savoir faire</font></a>
+				<li><br/><a href='#'><img src="../images/gif/S.gif"  style="margin-left:82px" onmouseover="this.src='../images/gif/S2.gif'" onmouseout="this.src='../images/gif/S.gif'" height='100px' width='100px'/></br><font color='white' style="margin-left: 89px">Savoir faire</font></a>
 				
 					<ul>
-						<li><a><font size='4'>Flyers</font></a>
-							<ul>
-								<li><a href='../savoirFaire/A4SF.php'><font size='4'>A4</font></a></li>
-								<li><a href='../savoirFaire/A5SF.php'><font size='4'>A5</font></a></li>
-								<li><a href='../savoirFaire/A6SF.php'><font size='4'>A6</font></a></li>
-							</ul>
-						</li>
-						<li><a href='../savoirFaire/logoSF.php'><font size='4'>Logos</font></a></li>
-						<li><a href='../savoirFaire/cartesSF.php'><font size='4'>Cartes de visites</font></a></li>
-						<li><a href='../savoirFaire/etiquetteSF.php'><font size='4'>Etiquettes</font></a></li>
-						<li><a href='../savoirFaire/chartesSF.php'><font size='4'>Chartes graphiques</font></a></li>
+						<div id="flyers">
+							<li><a><font size='4'>Flyers</font></a>
+								<ul>
+									<div id="A456">
+										<li><a href='../savoirFaire/A4SF.php'><font size='4'>A4</font></a></li>
+										<li><a href='../savoirFaire/A5SF.php'><font size='4'>A5</font></a></li>
+										<li><a href='../savoirFaire/A6SF.php'><font size='4'>A6</font></a></li>
+									</div>
+								</ul>
+							</li>
+						</div>
+						<div id="Logos"><li><a href='../savoirFaire/logoSF.php'><font size='4'>Logos</font></a></li></div>
+						<div id="Cartes"><li><a href='../savoirFaire/cartesSF.php'><font size='4'>Cartes de visites</font></a></li></div>
+						<div id="Etiquettes"><li><a href='../savoirFaire/etiquetteSF.php'><font size='4'>Etiquettes</font></a></li></div>
+						<div id="Chartes"><li><a href='../savoirFaire/chartesSF.php'><font size='4'>Chartes graphiques</font></a></li></div>
 					</ul>
 				</li>
 			
@@ -47,27 +51,30 @@
 				<li><br/><a href='#'><img src="../images/gif/ref.gif"  style="margin-left: 80px" onmouseover="this.src='../images/gif/Ref2.gif'" onmouseout="this.src='../images/gif/ref.gif'" height='100px' width='100px'/></br><font color='white' style="margin-left: 78px">Nos références</font></a>
 				
 					<ul>
-						<li><a><font size='4'>Flyers</font></a>
-							<ul>
-								<li><a href='../references/A4R.php'><font size='4'>A4</font></a></li>
-								<li><a href='../references/A5R.php'><font size='4'>A5</font></a></li>
-								<li><a href='../references/A6R.php'><font size='4'>A6</font></a></li>
-							</ul>
-						</li>
-						<li><a href='../references/logoR.php'><font size='4'>Logos</font></a></li>
-						<li><a href='../references/cartesR.php'><font size='4'>Cartes de visites</font></a></li>
-						<li><a href='../references/etiquetteR.php'><font size='4'>Etiquettes</font></a></li>
-						<li><a href='../references/chartesR.php'><font size='4'>Chartes graphiques</font></a></li>
+						<div id="flyers">
+							<li><a><font size='4'>Flyers</font></a>
+								<ul>
+									<div id="A456">
+										<li><a href='../references/A4R.php'><font size='4'>A4</font></a></li>
+										<li><a href='../references/A5R.php'><font size='4'>A5</font></a></li>
+										<li><a href='../references/A6R.php'><font size='4'>A6</font></a></li>
+									</div>
+								</ul>
+							</li>
+						</div>
+						<div id="Logos"><li><a href='../references/logoR.php'><font size='4'>Logos</font></a></li></div>
+						<div id="Cartes">	<li><a href='../references/cartesR.php'><font size='4'>Cartes de visites</font></a></li></div>
+						<div id="Etiquettes">	<li><a href='../references/etiquetteR.php'><font size='4'>Etiquettes</font></a></li></div>
+						<div id="Chartes">	<li><a href='../references/chartesR.php'><font size='4'>Chartes graphiques</font></a></li></div>
 					</ul>
 				</li>
-				
 				<!--menu contact-->
 				
 				<li><br/><a href='../contacter/formulaireContact2.php'><img src="../images/gif/crea.gif"  style="margin-left: 77px" onmouseover="this.src='../images/gif/crea2.gif'" onmouseout="this.src='../images/gif/crea.gif'" height='100px' width='100px'/></br><font color='white' style="margin-left: 100px">Contact</font></a>
 					
 					<ul>
-						<li><a href='../contacter/formulaireContact2.php'><font size='4'>Formulaire contact</font></a></li>
-						<li><a href='../contacter/formulaireContact2.php#situer'><font size='4'>Situer l'entreprise</font></a></li>
+						<div id="flyers"><li><a href='../contacter/formulaireContact2.php'><font size='4'>Formulaire contact</font></a></li></div>
+						<div id="Logos"><li><a href='../contacter/formulaireContact2.php#situer'><font size='4'>Situer l'entreprise</font></a></li></div>
 					</ul>	
 				</li>
 			
