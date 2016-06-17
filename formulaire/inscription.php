aa98c0d2e088cb26bddd8c8fa6e5c26ac34c261d
A2MI <A2MI.versionning@gmail.com>
2016-06-17 10:54:28 +0200

    fichier inscription.php

---
+++ formulaire/inscription.php
@@ -0,0 +1,26 @@
+<?php
+if(!empty($_POST['pseudo']))
+{
+// D'abord, je me connecte à la base de données.
+mysql_connect("localhost", "root", "");
+mysql_select_db("nom_db");
+
+// Je mets aussi certaines sécurités ici…
+$passe = mysql_real_escape_string(htmlspecialchars($_POST['passe']));
+$passe2 = mysql_real_escape_string(htmlspecialchars($_POST['passe2']));
+if($passe == $passe2)
+{
+$pseudo = mysql_real_escape_string(htmlspecialchars($_POST['pseudo']));
+$email = mysql_real_escape_string(htmlspecialchars($_POST['email']));
+// Je vais crypter le mot de passe.
+$passe = sha1($passe);
+
+mysql_query("INSERT INTO validation VALUES('', '$pseudo', '$passe', '$email')");
+}
+ 
+else
+{
+echo 'Les deux mots de passe que vous avez rentrés ne correspondent pas…';
+}
+}
+?>
