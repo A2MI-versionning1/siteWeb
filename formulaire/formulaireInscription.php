bcdd1877429d455e7a0b72da4bb75e4e1e9bc89e
A2MI <A2MI.versionning@gmail.com>
2016-06-17 10:28:23 +0200

    base de données

--- formulaire/formulaireInscription.php
+++ formulaire/formulaireInscription.php
@@ -30,22 +30,22 @@
 			
 			<center>
 				<h2>Formulaire d'inscription client</h2>
-				<form  method="post" action="">
+				<form  method="post" action="traitement.php">
 					<p><i>Complétez le formulaire. Les champs marqué par </i><em>*</em> sont <em>obligatoires</em></p>
 					<fieldset>
 						<legend>Informations personnelles</legend>
 						<label for="nom">Votre Nom <em>*</em></label><br>
-						<input id="nom" placeholder="ex: Fouillen" required=""><br><br>
+						<input id="nom" name="nom" placeholder="ex: Fouillen" required=""><br><br>
 						<label for="prenom">Votre Prénom <em>*</em></label><br>
-						<input id="prenom" placeholder="ex: Michel" required=""><br><br>
+						<input id="prenom" name="prenom" placeholder="ex: Michel" required=""><br><br>
 						<label for="societe">Votre société</label><br>
-						<input id="societe" placeholder="ex: A2MI" ><br><br>
+						<input id="societe" name="societe" placeholder="ex: A2MI" ><br><br>
 						<label for="adresse">Votre adresse <em>*</em></label><br>
-						<input id="adresse" placeholder="ex: rue de Suède" required=""><br><br>
+						<input id="adresse" name="adresse" placeholder="ex: rue de Suède" required=""><br><br>
 						<label for="codePostal">Votre code postal <em>*</em></label><br>
-						<input id="codePostal" placeholder="ex: 17000" required=""><br><br>
+						<input id="codePostal" name="codePostal" placeholder="ex: 17000" required=""><br><br>
 						<label for="ville">Votre ville <em>*</em></label><br>
-						<input id="ville" placeholder="ex: La Rochelle" required=""><br><br>
+						<input id="ville" name="ville" placeholder="ex: La Rochelle" required=""><br><br>
 						<label for="password">Votre mot de passe <em>*</em></label><br>
 						<input type="password" name="passe" minlength="8" placeholder="******" required=""/><br><br>
 						<label>Confirmation du mot de passe: <em>*</em></label><br>
@@ -53,10 +53,10 @@
 					</fieldset>
 					<fieldset>
 						<legend>Contact</legend>
-						<label for="telephone">Votre numéro de téléphone</label><br>
-						<input id="telephone" type="tel" placeholder="06xxxxxxxx" pattern="06[0-9]{8}"><br><br>
-						<label for="email">Votre adresse Email<em>*</em></label><br>
-						<input id="email" type="email" placeholder="****@A&M.com" required=""><br><br>
+						<label for="tel">Votre numéro de téléphone</label><br>
+						<input id="tel" name="tel" type="tel" placeholder="06xxxxxxxx" pattern="06[0-9]{8}"><br><br>
+						<label for="mail">Votre adresse Email<em>*</em></label><br>
+						<input id="mail" name="mail" type="email" placeholder="****@A&M.com" required=""><br><br>
 					</fieldset>
 					<p><input type="submit" value="S'inscrire"></p>
 				</form>
